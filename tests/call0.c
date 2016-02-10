614932b3821b8ba60a1d96ff89a6551c4e57c568
Pierre Pronchery <khorben@defora.org>
2016-02-10 05:18:36 +0100

    First semi-functional call on NetBSD/amd64

---
+++ tests/call0.c
@@ -0,0 +1,67 @@
+/* $Id$ */
+/* Copyright (c) 2016, Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS System libMarshall */
+/* All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+
+
+#include <stdio.h>
+#include "System/Marshall.h"
+
+#ifndef PROGNAME
+# define PROGNAME	"call0"
+#endif
+
+
+/* private */
+/* prototypes */
+static int _call0(void);
+
+
+/* functions */
+/* call0 */
+static int _call0(void)
+{
+	int ret = 0;
+
+	fprintf(stderr, "%s: %s() => %d\n", PROGNAME, __func__, ret);
+	return ret;
+}
+
+
+/* public */
+/* functions */
+/* main */
+int main(void)
+{
+	Variable * res;
+	int32_t r = -1;
+
+	res = variable_new(VT_INT32, &r);
+	marshall_call(res, (MarshallCallback)_call0, 0, NULL);
+	variable_get_as(res, VT_INT32, &r);
+	return (res == 0) ? 0 : 2;
+}
