79ae2dfd5332ad05b5b8daa76edc053110dd5b45
Pierre Pronchery <khorben@defora.org>
2025-11-20 01:22:33 +0100

    tests: improve the output on macOS

--- tests/tests.sh
+++ tests/tests.sh
@@ -21,6 +21,9 @@
 PROGNAME="tests.sh"
 #executables
 DATE="date"
+ECHO="echo"
+UNAME="uname"
+[ $($UNAME -s) != "Darwin" ] || ECHO="/bin/echo"
 
 
 #functions
@@ -50,7 +53,7 @@ _run()
 	[ $# -eq 1 ] || sep=" "
 
 	shift
-	echo -n "$test:" 1>&2
+	$ECHO -n "$test:" 1>&2
 	(echo
 	echo "Testing: $test" "$@"
 	testexe="./$test"
